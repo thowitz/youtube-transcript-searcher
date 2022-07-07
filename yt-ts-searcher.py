0dcecb4c8dd38056656641b058c1dfa86e5a4ef9
thowitz <timjhowitz@gmail.com>
2022-07-07 23:19:10 +0100

    Added scrapetube and transcript api

--- yt-ts-searcher.py
+++ yt-ts-searcher.py
@@ -1,5 +1,22 @@
+import scrapetube
+from youtube_transcript_api import YouTubeTranscriptApi
+
+numberOfVideosToSearch = 2
+
+
 def main():
-    pass
+    allVideos = scrapetube.get_channel("UC4JX40jDee_tINbkjycV4Sg")
+    videoIds = []
+
+    counter = 0
+    for video in allVideos:
+        if counter > numberOfVideosToSearch:
+            break
+
+        videoIds.append(video["videoId"])
+        counter += 1
+
+    transcripts = YouTubeTranscriptApi.get_transcripts(videoIds)
 
 
 main()
