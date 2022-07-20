8d80878a92b6fc6f40143f84057df6958356dec8
thowitz <timjhowitz@gmail.com>
2022-07-20 22:07:17 +0100

    Switched to using enumerate cause I just found out what it does, plus it's cool and elegant

--- yt-ts-searcher.py
+++ yt-ts-searcher.py
@@ -8,13 +8,11 @@ def main():
     allVideos = scrapetube.get_channel("UC4JX40jDee_tINbkjycV4Sg")
     videoIds = []
 
-    counter = 0
-    for video in allVideos:
-        if counter > numberOfVideosToSearch:
+    for videoIndex, video in enumerate(allVideos):
+        if videoIndex >= numberOfVideosToSearch:
             break
 
         videoIds.append(video["videoId"])
-        counter += 1
 
     transcripts = YouTubeTranscriptApi.get_transcripts(videoIds)
 
